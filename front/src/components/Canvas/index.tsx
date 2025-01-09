1796e5b37286259366d1d5c0a56b42c6a7311c9a
Peter Krive <thepetruha@Peters-MacBook-Pro.local>
2025-01-09 17:52:00 +0400

    feat: Add first sprite of player

--- front/src/components/Canvas/index.tsx
+++ front/src/components/Canvas/index.tsx
@@ -1,5 +1,6 @@
 import { Stage, Container, Sprite, Text } from '@pixi/react';
 import styled from 'styled-components';
+import * as PIXI from "pixi.js";
 
 const Wrapper = styled.div`
     overflow: hidden;
@@ -7,13 +8,20 @@ const Wrapper = styled.div`
     width: 100%;
 `
 
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
+
 export default function GameCanvas({ size }: { size: number }) {
-    const bunnyUrl = 'https://pixijs.io/pixi-react/img/bunny.png';
+    // Загружаем текстуру из изображения
+    const baseTexture = PIXI.BaseTexture.from("src/assets/bomberman.png");
+
+    // Указываем координаты и размеры нужного куска
+    const frame = new PIXI.Rectangle(0, 0, 15, 15); // x, y, width, height
+    const texture = new PIXI.Texture(baseTexture, frame);
 
     return (
         <Wrapper style={{ maxWidth: size, maxHeight: size }}>
-            <Stage width={size} height={size} options={{ background: 0x1099bb }}>
-                <Sprite image={bunnyUrl} x={300} y={150} />
+            <Stage width={size} height={size} options={{ background: 0x43850F }}>
+                <Sprite texture={texture} x={150} y={150} anchor={0.5} scale={4} />
             </Stage>
         </Wrapper>
     )
