4f4900d9cd8474eb45e079c736b5fe20928276e4
Peter Krive <thepetruha@Peters-MacBook-Pro.local>
2025-01-09 18:12:08 +0400

    feat: Add base generator map (without detect colisions)

--- front/src/components/Canvas/index.tsx
+++ front/src/components/Canvas/index.tsx
@@ -10,6 +10,27 @@ const Wrapper = styled.div`
 
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
 
+function GameMap({ size }: { size: number }) {
+    // Загружаем текстуру из изображения
+    const baseTexture = PIXI.BaseTexture.from("src/assets/bomberman.png");
+
+    // Указываем координаты и размеры нужного куска
+    const frame = new PIXI.Rectangle(48, 48, 16, 18); // x, y, width, height
+    const texture = new PIXI.Texture(baseTexture, frame);
+
+    const ar = [[...Array(10)], [...Array(10)]]
+
+    return (
+        ar.map((_e, i) => {
+            return (
+                _e.map((_el, j) => {
+                    return <Sprite texture={texture} x={i * 100} y={j * 120} anchor={0} scale={4} />
+                })
+            ) 
+        })
+    )
+}
+
 export default function GameCanvas({ size }: { size: number }) {
     // Загружаем текстуру из изображения
     const baseTexture = PIXI.BaseTexture.from("src/assets/bomberman.png");
@@ -22,6 +43,7 @@ export default function GameCanvas({ size }: { size: number }) {
         <Wrapper style={{ maxWidth: size, maxHeight: size }}>
             <Stage width={size} height={size} options={{ background: 0x43850F }}>
                 <Sprite texture={texture} x={150} y={150} anchor={0.5} scale={4} />
+                <GameMap size={size} />
             </Stage>
         </Wrapper>
     )
